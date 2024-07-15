e3d96057f34e59d89728010a407c745aec3212c8
Ali Nabavi <arz1379n@gmail.com>
2024-07-15 13:59:21 +0330

    Implement HorizontalPager for fixing animations

--- app/src/main/java/com/sali/habitino/view/screen/MainScreen.kt
+++ app/src/main/java/com/sali/habitino/view/screen/MainScreen.kt
@@ -1,10 +1,7 @@
 package com.sali.habitino.view.screen
 
-import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,6 +10,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -20,6 +19,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -33,10 +33,13 @@ import com.sali.habitino.view.component.RemoteHabitsList
 import com.sali.habitino.view.component.SelfAddedHabitsList
 import com.sali.habitino.view.theme.LightBlue
 import com.sali.habitino.viewmodel.ScoreStateViewModel
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MainHabitScreen() {
-    var habitListEnabled by remember { mutableIntStateOf(0) }
+    val pagerState = rememberPagerState(initialPage = 0, pageCount = { 2 })
+    val coroutineScope = rememberCoroutineScope()
 
     val scoreStateViewModel: ScoreStateViewModel = hiltViewModel()
     val scoreState by scoreStateViewModel.score.collectAsState()
@@ -59,17 +62,21 @@ fun MainHabitScreen() {
             HabitTypeItem(
                 modifier = Modifier.weight(0.33f),
                 text = stringResource(R.string.common_habits),
-                enabled = habitListEnabled == 0
+                enabled = pagerState.currentPage == 0
             ) {
-                habitListEnabled = 0
+                coroutineScope.launch {
+                    pagerState.animateScrollToPage(0, animationSpec = tween(500))
+                }
             }
 
             HabitTypeItem(
                 modifier = Modifier.weight(0.33f),
                 text = stringResource(R.string.self_added),
-                enabled = habitListEnabled == 1
+                enabled = pagerState.currentPage == 1
             ) {
-                habitListEnabled = 1
+                coroutineScope.launch {
+                    pagerState.animateScrollToPage(1, animationSpec = tween(500))
+                }
             }
         }
 
@@ -85,30 +92,8 @@ fun MainHabitScreen() {
             textAlign = TextAlign.Start
         )
 
-        AnimatedContent(
-            targetState = habitListEnabled,
-            label = stringResource(R.string.habits_list),
-            transitionSpec = {
-                if (targetState == 0) {
-                    slideInHorizontally(
-                        animationSpec = tween(500),
-                        initialOffsetX = { fullWidth -> -fullWidth }
-                    ) togetherWith slideOutHorizontally(
-                        animationSpec = tween(500),
-                        targetOffsetX = { fullWidth -> fullWidth }
-                    )
-                } else {
-                    slideInHorizontally(
-                        animationSpec = tween(500),
-                        initialOffsetX = { fullWidth -> fullWidth }
-                    ) togetherWith slideOutHorizontally(
-                        animationSpec = tween(500),
-                        targetOffsetX = { fullWidth -> -fullWidth }
-                    )
-                }
-            }
-        ) { targetState ->
-            when (targetState) {
+        HorizontalPager(state = pagerState) { page ->
+            when (page) {
                 0 -> {
                     RemoteHabitsList {
                         score += it
