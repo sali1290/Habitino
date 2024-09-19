7f657344f9ab470395c461784186704b7044a128
Ali Nabavi <arz1379n@gmail.com>
2024-09-19 22:37:53 +0330

    feat: Show dialog when app open a saved app

--- app/src/main/java/com/sali/habitino/view/service/AppCheckerService.kt
+++ app/src/main/java/com/sali/habitino/view/service/AppCheckerService.kt
@@ -5,38 +5,53 @@ import android.app.AlertDialog
 import android.util.Log
 import android.view.WindowManager
 import android.view.accessibility.AccessibilityEvent
+import com.sali.habitino.model.dto.SavedApp
+import com.sali.habitino.model.repo.AppsRepo
 import com.sali.habitino.view.utile.LogKeys
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-
+@AndroidEntryPoint
 class AppCheckerService : AccessibilityService() {
 
     private var isDialogShown = false
     private var lastDialogTime: Long = 0
     private val dialogInterval = 5000 // 5 seconds
 
+    @Inject
+    lateinit var appsRepo: AppsRepo
+    private var savedApps = emptyList<SavedApp>()
+
     override fun onAccessibilityEvent(event: AccessibilityEvent) {
+        CoroutineScope(Dispatchers.IO).launch {
+            savedApps = appsRepo.getAllSavedApps()
+        }
         if (event.eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
             val packageName = event.packageName.toString()
             Log.d(LogKeys.ACCESSIBILITY_SERVICE, "Package Name: $packageName")
             // Implement checking the package name of saved apps by user by the app is now open
             if (!isDialogShown) {
                 val currentTime = System.currentTimeMillis()
-                if (
-                    currentTime - lastDialogTime > dialogInterval &&
-                    packageName == "com.google.android.dialer"
-                ) {
-                    isDialogShown = true
-                    lastDialogTime = currentTime
-                    showDialog()
+                if (currentTime - lastDialogTime > dialogInterval) {
+                    savedApps.forEach {
+                        if (it.packageName == packageName) {
+                            isDialogShown = true
+                            lastDialogTime = currentTime
+                            showDialog(it.message)
+                        }
+                    }
                 }
             }
         }
     }
 
-    private fun showDialog() {
+    private fun showDialog(message: String) {
         val builder = AlertDialog.Builder(applicationContext)
-        builder.setTitle("Alert")
-            .setMessage("You are entering the dialer app")
+        builder.setTitle("Be aware!")
+            .setMessage(message)
             .setPositiveButton("OK") { _, _ ->
                 isDialogShown = false // Reset the flag when the dialog is dismissed
             }
